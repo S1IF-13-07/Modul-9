a75d22fdf9d6ec324a5daf82f3f98287b25bd99f
IsmailMarasabesy <faizismail749@gmail.com>
2025-11-17 10:57:37 +0700

    Add files via upload

---
+++ ismailmarasabessy/lat1.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	var orang int
+	fmt.Print("Masukkan jumlah orang: ")
+	fmt.Scan(&orang)
+
+	motor := orang / 2
+
+	if orang%2 != 0 {
+		motor = motor + 1
+	}
+
+	fmt.Println("Jumlah motor yang diperlukan:", motor)
+}
