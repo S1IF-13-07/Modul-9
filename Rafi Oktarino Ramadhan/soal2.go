d73498f3dc84cb3f0b4a3fecc6952682c1adab0d
rafioktars <152888181+rafioktars@users.noreply.github.com>
2025-11-19 09:00:40 +0700

    Add files via upload

---
+++ Rafi Oktarino Ramadhan/soal2.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	var bilangan int
+	var teks string
+	fmt.Scan(&bilangan)
+	if bilangan%2 == 0 && bilangan < 0 {
+		teks = "genap negatif"
+	} else {
+		teks = "bukan"
+	}
+	fmt.Println(teks)
+}
